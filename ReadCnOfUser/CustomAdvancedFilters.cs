bae8e4d77f12c7a88cd297fbb7ce89f758424fb4
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:06:05 +0530

    Add project files.

---
+++ ReadCnOfUser/CustomAdvancedFilters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+
+namespace ReadCnOfUser
+{
+    /// <summary>
+    /// This advanced filters object allows custom attributes to be set for the filter principal before performing a search 
+    /// </summary>
+    public class CustomAdvancedFilters : AdvancedFilters
+    {
+        /// <summary>
+        /// Creates a new CustomAdvancedFilters object
+        /// </summary>
+        /// <param name="principal">The filter principal</param>
+        public CustomAdvancedFilters(Principal principal): base(principal) { }
+
+        /// <summary>
+        /// Sets a custom attribute for the filter principal
+        /// </summary>
+        /// <param name="attribute">The attribute to set</param>
+        /// <param name="value">The attribute value</param>
+        /// <param name="objectType">The type of the value</param>
+        /// <param name="matchType">Indicates how to match the result</param>
+        public void CustomPropertySet(string attribute, object value, Type objectType, MatchType matchType)
+        {
+            // Set the custom property
+            AdvancedFilterSet(attribute, value, objectType, matchType);
+        }
+    }
+}
---
+++ ReadCnOfUser/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+
+namespace ReadCnOfUser
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string userWithCn = "David B. Starcevich";
+            Console.WriteLine("Let's get sAMAccountName of user with cn as {0}", userWithCn);
+
+            var user = FindUser("cn", userWithCn, "DC1.domain.com", "Administrator", "Pass99");
+            if (user != null)
+            {
+                Console.WriteLine("We got sAMAccountName of the user as '{0}'", user.SamAccountName);
+            }
+            else
+            {
+                Console.WriteLine("User not found.");
+            }
+
+            // Awaiting for quit
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Finds a user by quering a property value
+        /// </summary>
+        /// <param name="queryProperty">The property to query</param>
+        /// <param name="propertyValue">The property value to match</param>
+        /// <param name="domainController">The domain controller's address</param>
+        /// <param name="username">The username</param>
+        /// <param name="password">The password</param>
+        /// <param name="container">The destination's ou path</param>
+        /// <returns>The user principal returned by the search</returns>
+        public static UserPrincipal FindUser(string queryProperty, object propertyValue, string domainController, string username, string password, string container = null)
+        {
+            // Create the principal context
+            var principalContext = string.IsNullOrEmpty(container) ? new PrincipalContext(ContextType.Domain, domainController, username, password) : new PrincipalContext(ContextType.Domain, domainController, container, username, password);
+
+            // Create the query filter user principal
+            var filterPrincipal = new UserPrincipal(principalContext);
+
+            // Create an advanced filters object so that we can set a custom attribute
+            var customFilter = new CustomAdvancedFilters(filterPrincipal);
+            customFilter.CustomPropertySet(queryProperty, propertyValue, propertyValue.GetType(), MatchType.Equals);
+
+            // Create the principal searcher
+            var principalSearcher = new PrincipalSearcher(filterPrincipal);
+
+            // Find the principal
+            var principal = principalSearcher.FindOne();
+
+            // Cast it to user principal
+            var userPrincipal = principal as UserPrincipal;
+
+            return userPrincipal;
+        }
+    }
+}
